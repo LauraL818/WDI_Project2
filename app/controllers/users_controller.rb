d6e04d032a6be52f227ae03becd2166cd5e4b649
Laura Leonard <Laura.Leonard818@gmail.com>
2016-02-22 14:50:02 -0800

    updated views and made avatar functional

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -38,6 +38,6 @@ class UsersController < ApplicationController
 
   private
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :username, :email, :password)
+    params.require(:user).permit(:first_name, :last_name, :username, :email, :favorite, :avatar, :password)
   end
 end
