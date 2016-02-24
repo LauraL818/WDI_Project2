145b4c3021b0d8be5d57c5fb28fa941c322a1ae5
Laura Leonard <Laura.Leonard818@gmail.com>
2016-02-23 19:03:01 -0800

    alerts below navbar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,7 +16,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to users_path(@user)
+      redirect_to new_session_path
+      flash[:success] = "Welcome! Please login to access your account."
     end
   end
 
