a334d5daa02e5936b49c53d66ad2f1bec9b57989
Laura Leonard <Laura.Leonard818@gmail.com>
2016-02-25 21:29:05 -0800

    added all stadium photos and finished styling

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,11 +8,14 @@
 
 
 
+
 Stadium.create({name:"Angel Stadium of Anaheim", team:"Los Angeles Angels of Anaheim", location:"Anaheim, CA", photo:"angels.jpg", history:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim .", year:1966, capacity: 41503, feature:"Food Court"})
+
+Stadium.create({name:"AT&T Ballpark", team:"San Francisco Giants", location:"San Francisco, CA", photo:"giants.jpg", history:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim .", year:2000, capacity: 41915, feature:"Food Court"})
 
 Stadium.create({name:"Busch Stadium", team:"St. Louis Cardinals", location:"St. Louis, MO", photo:"cardinals.jpg", history:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim .", year:2006, capacity: 43975, feature:"Food Court"})
 
-Stadium.create({name:"Chase Field", team:"Arizona Diamondbacks", location:"Phoenix, AZ", photo:"giants.jpg", history:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim.", year:1998, capacity: 48519, feature:"The View"})
+Stadium.create({name:"Chase Field", team:"Arizona Diamondbacks", location:"Phoenix, AZ", photo:"diamondbacks.jpg", history:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim.", year:1998, capacity: 48519, feature:"The View"})
 
 Stadium.create({name:"Citi Field", team:"New York Mets", location:"Queens, NY", photo:"mets.jpg", history:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim .", year:2009, capacity: 41922, feature:"Food Court"})
 
