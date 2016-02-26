da29575820ea7bfa636e5ee5017aa81fd14348b9
Laura Leonard <Laura.Leonard818@gmail.com>
2016-02-25 18:08:40 -0800

    organized css file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,28 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Stadium.create({name:"AT&T Ballpark", team:"San Francisco Giants", location:"San Francisco, CA", photo:"giants.jpg", history:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim .", year:2000, capacity: 41503, feature:"Food Court"})
+
+Stadium.destroy_all
+Stadium.create({name:"Angel Stadium of Anaheim", team:"Los Angeles Angels of Anaheim", location:"Anaheim, CA", photo:"angels.jpg", history:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim .", year:1966, capacity: 41503, feature:"Food Court"})
+
+# Stadium.create({name:"Busch Stadium", team:"St. Louis Cardinals", location:"St. Louis, MO", photo:"cardinals.jpg", history:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim .", year:2006, capacity: 43975, feature:"Food Court"})
+#
+# Stadium.create({name:"Chase Field", team:"Arizona Diamondbacks", location:"Phoenix, AZ", photo:"giants.jpg", history:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim.", year:1998, capacity: 48519, feature:"The View"})
+#
+# Stadium.create({name:"Citi Field", team:"New York Mets", location:"Queens, NY", photo:"mets.jpg", history:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim .", year:2009, capacity: 41922, feature:"Food Court"})
+#
+# Stadium.create({name:"Citizens Bank Park", team:"Philadelphia Phillies", location:"Philadelphia, PA", photo:"phillies.jpg", history:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim .", year:2004, capacity: 43651, feature:"Food Court"})
+#
+# Stadium.create({name:"Comerica Park", team:"Detroit Tigers", location:"Detroit, MI", photo:"tigers.jpg", history:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. m ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim .", year:2000, capacity: 41574, feature:"Food Court"})
+#
+# Stadium.create({name:"Comerica Park", team:"Detroit Tigers", location:"Detroit, MI", photo:"tigers.jpg", history:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. m ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim .", year:2000, capacity: 41574, feature:"Food Court"})
+#
+# Stadium.create({name:"Comerica Park", team:"Detroit Tigers", location:"Detroit, MI", photo:"tigers.jpg", history:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. m ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim .", year:2000, capacity: 41574, feature:"Food Court"})
+#
+# Stadium.create({name:"Comerica Park", team:"Detroit Tigers", location:"Detroit, MI", photo:"tigers.jpg", history:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. m ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim .", year:2000, capacity: 41574, feature:"Food Court"})
+#
+# Stadium.create({name:"Comerica Park", team:"Detroit Tigers", location:"Detroit, MI", photo:"tigers.jpg", history:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. m ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim .", year:2000, capacity: 41574, feature:"Food Court"})
+#
+# Stadium.create({name:"Comerica Park", team:"Detroit Tigers", location:"Detroit, MI", photo:"tigers.jpg", history:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. m ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim .", year:2000, capacity: 41574, feature:"Food Court"})
+#
+# Stadium.create({name:"Comerica Park", team:"Detroit Tigers", location:"Detroit, MI", photo:"tigers.jpg", history:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. m ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim .", year:2000, capacity: 41574, feature:"Food Court"})
