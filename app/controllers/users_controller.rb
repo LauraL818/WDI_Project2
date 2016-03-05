34dc60078de354750339dbffcc8663ced00e8bd0
Laura Leonard <Laura.Leonard818@gmail.com>
2016-03-05 12:27:18 -0800

    fixed if user email taken route to different view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,8 +16,11 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to new_session_path
+      redirect_to users_path
       flash[:success] = "Welcome! Please login to access your account."
+    else
+      redirect_to new_user_path
+      flash[:failure] = "Email already taken please try again"
     end
   end
 
